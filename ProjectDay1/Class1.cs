18cc4adc7ff42eae4e16b4f908d8830a39226d3a
u31d62 <u31d62@INGURWN110544>
2023-10-31 17:14:38 +0530

    Add project files.

---
+++ ProjectDay1/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDay1
+{
+    public class Account
+    {
+       
+        int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        String accountType;
+        public String AccountType
+        {
+            get
+            {
+                return accountType;
+            }
+            set
+            {
+                accountType = value;
+            }
+        }
+        double balance;
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+        public bool WithDraw(double amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+        public String GetDetails()
+        {
+            StringBuilder s = new StringBuilder();
+
+            s.Append($"Id is :{id} \n");
+            s.Append($"Balance is :{balance} \n");
+            s.Append($"AccountType is :{accountType} \n");
+            return Convert.ToString(s);
+
+        }
+        public Account()
+        {
+            id = 0;
+            balance = 0;
+            accountType = "";
+        }
+    }
+}
---
+++ ProjectDay1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace ProjectDay1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account();
+            Console.Write("enter id : ");
+            a.Id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("enter balance : ");
+            a.Balance = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter Account Type : ");
+            a.AccountType = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("calling get details \n");
+            String s = a.GetDetails();
+            Console.WriteLine(s);
+            Console.WriteLine("calling withdraw \n");
+            a.WithDraw(200);
+            Console.WriteLine("calling get deatils \n");
+            String s2 = a.GetDetails();
+            Console.WriteLine(s2);     
+        }
+    }
+}
